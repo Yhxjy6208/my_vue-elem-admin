662601a2668b3722c5b5853aaae7516e6efc02d5
Yhxjy6208 <1064305891@qq.com>
2023-03-16 08:28:57 +0800

    新增了手动按需导入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Elementui from "./plugins/elementui";
+// import Login from './views/account/Login.vue';
+// import Elementui from "./plugins/elementui";
 
 // import { ElButton } from "element-plus";
 // import 'element-plus/es/components/button/style/css'
@@ -12,4 +13,5 @@ import Elementui from "./plugins/elementui";
 // import 'element-plus/dist/index.css'
 // createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
 
-createApp(App).use(store).use(router).use(Elementui).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
+// createApp(Login).use(store).use(router).mount("#app");
