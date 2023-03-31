71b423ca8fd34a3f7e302b4cf9c186eea5f3f7ec
Yhxjy6208 <1064305891@qq.com>
2023-03-31 13:31:35 +0800

    vuex 状态守护

--- src/views/console/Index.vue
+++ src/views/console/Index.vue
@@ -1,13 +1,25 @@
 <template>
-    <svg-icon></svg-icon>
+    <div>{{ value }}</div>
 
 </template>
 <script>
     //引入图标只在本文件生效 局部引用
-    import { Avatar } from '@element-plus/icons-vue';
-    export default{
-        components:{
-    Avatar
-}
+//     import { Avatar } from '@element-plus/icons-vue';
+//     export default{
+//         components:{
+//     Avatar
+// }
+//     }
+import {useStore} from 'vuex';
+export default{
+    setup(){
+        const store = useStore();//多一层少一层
+        const {state} = useStore();
+        const value = state.app.count
+        const value_store = store.state.app.count
+        return{
+            value,value_store
+        }
     }
+}
 </script>
