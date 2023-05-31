290577bc34d2cc62646923ba5d74a3753efc952b
Yhxjy6208 <1064305891@qq.com>
2023-05-31 11:47:35 +0800

    loading加载 插槽

--- src/views/info/Index.vue
+++ src/views/info/Index.vue
@@ -36,7 +36,16 @@
     <BasisTable :coulums="table_config.table_header" 
     :config="table_config.config"
     :request = "table_config.request"
-    ></BasisTable>
+    >
+    <template v-slot:operation="sloat_data">
+        <el-button type="danger" size="small" 
+            @click="handleDetailed(sloat_data.data.id)">编辑
+        </el-button>
+        <el-button type="danger" size="small" 
+            @click="handleDeleteConfirm(sloat_data.data.id)">删除
+        </el-button>
+    </template>
+    </BasisTable>
     <!-- <el-table 
     ref="table"
     border
@@ -97,8 +106,8 @@ export default{
         const table_config = reactive({
             //表头以及数据绑定标签名
             table_header:[
-                {label:"标题",prop:"title"},
-                {label:"类别",prop:"category_name"},
+                {label:"标题",prop:"title",width:"500"},
+                {label:"类别",prop:"category_name",width:"200"},
                 {label:"日期",prop:"create_date",type:"function",callback:(row)=>{
                     return getDate({value:row.createDate*1000})
                 }},
@@ -108,8 +117,22 @@ export default{
                     type:"switch",
                     key_id:"id",
                     api_module:"info",
-                    api_key:"info_status"
+                    api_key:"info_status",
+                    width:"100"
                 },
+                {
+                    label:"操作",
+                    type:"slot",
+                    sloat_name:"operation",
+                    width:"200",
+                    delete_elem:true
+                },
+                {
+                    label:"其他插槽",
+                    type:"slot",
+                    sloat_name:"other",
+                    width:"200"
+                }
             ],
             //自定义配置
             config:{
